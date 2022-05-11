6ab73d9a2450201b16534992b74aa2656133b022
Lucroz <Lucroz@lucroz.com>
2022-05-11 22:14:41 +0200

    lab-10, create a debian11 VM that install docker

---
+++ Vagrant/lab-10/install_docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo apt -y update
+
+# install docker
+sudo apt install -y git curl
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+sudo usermod -aG docker vagrant
+sudo systemctl enable docker
+sudo systemctl start docker
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+echo "For this Stack, you will use $(ip -f inet addr show enp0s8 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p') IP Address"
